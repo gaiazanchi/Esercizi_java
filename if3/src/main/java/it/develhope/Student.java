586f0d16f4e35ceeeaf9e339ce7b3f6384d10862
Gaia Zanchi <gaia.zanchi2001@gmail.com>
2022-12-09 10:28:13 +0100

    Ho aggiunto commenti a if3 e if4

--- if3/src/main/java/it/develhope/Student.java
+++ if3/src/main/java/it/develhope/Student.java
@@ -4,21 +4,27 @@ import java.util.Random;
 
 public class Student {
 
+    //instance variables
     String name;
     int age;
 
+    //constructor method
     public Student(String studentName, int studentAge){
         name = studentName;
         age = studentAge;
     }
 
+    //method for guessing the age of the person
     public void guessingAge(){
 
+        //genero un numero casuale intero usando java.util.Random, ho cercato come fare
         Random random = new Random();
         int number = random.nextInt(35);
 
+        //stampo il numero randomicamente generato
         System.out.println("I generated the random number " + number + " for student " + this.name);
 
+        //stampo cose diverse a seconda se il numero generato sia >/</= all'età dello studente
         if(number > this.age){
             System.out.println("The age of the student is less than " + number);
         }else if(number == this.age){
--- if4/src/main/java/it/develhope/Person.java
+++ if4/src/main/java/it/develhope/Person.java
@@ -2,21 +2,27 @@ package it.develhope;
 
 public class Person {
 
+    //instance variable
     int age;
 
+    //constructor method
     public Person(){
 
         int max = 80;
         int min = 5;
 
+        // genero un numero casuale come chiesto e lo casto a intero, poi lo stampo
         int random = (int) Math.floor(Math.random()*(max - min + 1) + min);
         System.out.println("I generated the random number " + random);
 
+        //assegno all'età della persona l'intero random che ho generato
         this.age = random;
 
     }
 
+    //method
     public String getLifeStage(){
+        //metto uno stage life diverso a seconda dell'età della persona, facendo attenzione a non lasciare esclusi dei valori
         if(age <= 12)
             return "child";
         else if (age >= 13 && age <= 19)
